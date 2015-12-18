514725e47d3de60b3def9bc9ab77b07929ec7853
Ondrej Palenicek <ondrej.palenicek@gmail.com>
2015-12-18 19:29:48 +0100

    Added day 1.

---
+++ day1/src/main.rs
@@ -0,0 +1,31 @@
+#![feature(io)]
+
+use std::io;
+use std::io::Read;
+
+fn main() {
+    let mut cnt = 0;
+
+    let mut pos_in_input = 1;
+    let mut entered_basement_at_pos = -1;
+
+    for wrapped_ch in io::stdin().chars() {
+        
+        let ch = wrapped_ch.unwrap();
+
+        if ch == '(' {
+            cnt += 1;
+        } else if ch == ')' {
+            cnt -= 1;
+        }
+
+        if cnt == -1 && entered_basement_at_pos == -1 {
+            entered_basement_at_pos = pos_in_input;
+        }
+
+        pos_in_input += 1;
+    }
+
+    println!("The final floor number is: {}", cnt);
+    println!("First entered basement at pos: {}", entered_basement_at_pos);
+}
