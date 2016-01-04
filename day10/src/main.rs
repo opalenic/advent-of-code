a8d9f1cb2ef6b967346fa0f31d282cf8e158f73e
Ondrej Palenicek <ondrej.palenicek@gmail.com>
2016-01-04 03:01:16 +0100

    Added day 10.

---
+++ day10/src/main.rs
@@ -0,0 +1,35 @@
+extern crate itertools;
+
+use std::env::args;
+
+use itertools::Itertools;
+
+fn main() {
+
+    let mut a = args();
+
+    a.next(); // The first argument is the binary name/path
+
+    let start = a.next().unwrap().to_string(); // The puzzle input
+    let num_iterations = a.next().unwrap().parse::<u32>().unwrap(); // The number of iterations
+
+
+    let mut current = start.chars().map(|ch| ch.to_digit(10).unwrap() as u8).collect::<Vec<u8>>();
+
+    for i in 0..num_iterations {
+        if i % 5 == 0 {
+            println!("{} - {}", i, current.len());
+        }
+
+        current = current.into_iter()
+                         .group_by(|&ch| ch)
+                         .flat_map(|(key, matched)| vec![matched.len() as u8, key])
+                         .collect();
+
+    }
+
+
+    println!("The lenght of the string after {} iterations is: {}",
+             num_iterations,
+             current.len());
+}
